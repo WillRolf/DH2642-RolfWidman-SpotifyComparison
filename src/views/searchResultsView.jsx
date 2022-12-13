d3578a79653e483b02520c4a533ca2585e79c286
Alexander Widman <alexandio01@gmail.com>
2022-12-13 21:42:00 +0100

    Update searchResultsView.jsx

--- src/views/searchResultsView.jsx
+++ src/views/searchResultsView.jsx
@@ -1,32 +1,25 @@
 function SearchResultsView(props){
     function showSearchResultsCB(result){
-        console.log(result)
-        //const trackImage = result.data.visuals.avatarImage.sources[0].url;
-        //console.log(trackImage)
-        // result.data.name?result.data.albumOfTrack.coverArt.sources[0].url:trackImage
+        let image = result.data.name?
+                    result.data.albumOfTrack.coverArt.sources[0].url:
+                    result.data.visuals.avatarImage?.sources[0].url;
+        function getNameCB(){
+            if (image == null){ return }
+            else { 
+                return <div>
+                {result.data.name?result.data.name:result.data.profile.name}
+                </div>}}
         function onSearchResultACB(){ 
             props.onSearchResult(result);
             /* TODO window.location.hash = "#details"; */
         }
-        function returnCoverCB(){
-            return console.log("hello");
-            //const trackImage = result.data.visuals.avatarImage.sources[0].url;
-            //console.log(trackImage)
-            
-            if (result.data.profile.name){
-                console.log("hello")
-                return result.data.albumOfTrack.coverArt.sources[0].url
-            }
-            return result.data.visuals.avatarImage.sources[0].url
-        }
         return (
             <span class="searchResult"
             onClick={onSearchResultACB}>
-                <img src={result.data.name?result.data.albumOfTrack.coverArt.sources[0].url:result.data.visuals.avatarImage.sources[0].url}height="100">
+                <img src={image}
+                    height="100">
                 </img>
-                <div>
-                    {result.data.name?result.data.name:result.data.profile.name}
-                </div>
+                {getNameCB()}
             </span>
         );
     }
