3ab629728925a65da69f5dd39ef88c92c3803000
AlexWidman <119602587+AlexWidman@users.noreply.github.com>
2022-12-15 20:20:14 +0100

    Delete firebaseConfig.js

--- src/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export default{
-  apiKey: "AIzaSyBrXNBHgqZ9-_h16Dw_KoMVGRxU2BPpCdg",
-  authDomain: "rolfwidma-spotifycomparison.firebaseapp.com",
-  databaseURL: "https://rolfwidma-spotifycomparison-default-rtdb.europe-west1.firebasedatabase.app/",
-  projectId: "rolfwidma-spotifycomparison",
-  storageBucket: "rolfwidma-spotifycomparison.appspot.com",
-  messagingSenderId: "248410430504",
-  appId: "1:248410430504:web:1433e92fa71bbce559291d"
-};
