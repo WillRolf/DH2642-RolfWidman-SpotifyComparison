35bca451740627a40ba178cf05418df6476d6c67
Alexander Widman <alexandio01@gmail.com>
2022-12-22 17:38:58 +0100

    login progress

--- src/presenters/loginPresenter.jsx
+++ src/presenters/loginPresenter.jsx
@@ -1,6 +1,5 @@
 import { login, register } from "../firebaseModel.js";
 import LoginView from "../views/loginView.jsx";
-import RegisterView from "../views/registerView.jsx"
 import "../firebaseModel.js"
 
 const Login={
@@ -14,13 +13,11 @@ const Login={
         function registerUserACB(){ register({email: this.email, password: this.password, playlist: this.model.songs}) }
         function emailChangeACB(e){ this.email = e }
         function passwordChangeACB(p){ this.password = p }
-        return(<div>
+        return(
             <LoginView onLoginButton={ loginUserACB.bind(this) }
+                    onRegisterButton={ registerUserACB.bind(this) }
                     onEmailInput={ emailChangeACB.bind(this) }
                     onPasswordInput={ passwordChangeACB.bind(this) }/>
-            <RegisterView onRegisterButton={ registerUserACB.bind(this) }
-                    onEmailInput={ emailChangeACB.bind(this) }
-                    onPasswordInput={ passwordChangeACB.bind(this) }/></div>
         )
     }
 }
--- src/views/loginView.jsx
+++ src/views/loginView.jsx
@@ -1,19 +1,25 @@
 function LoginView(props){
     function onLoginButtonPressACB(){ props.onLoginButton(); }
+    function onRegisterButtonPressACB(){ props.onRegisterButton(); }
     function onEmailInputChangeACB(event){ props.onEmailInput(event.target.value); }
     function onPasswordInputChangeACB(event){ props.onPasswordInput(event.target.value); }
-    return (<div>
+    return (<div class="login">
+        <span class="loginTitle">Login or Register!</span><br></br>
         <input class="inputBox" style="border-color: rgb(25, 20, 20)" 
         placeholder="Email..."
         onChange={onEmailInputChangeACB}>
-        </input>
+        </input><br></br>
         <input class="inputBox" style="border-color: rgb(25, 20, 20)" 
         placeholder="Password..."
         onChange={onPasswordInputChangeACB}>
-        </input>
+        </input><br></br>
         <button class="button"
         onClick={onLoginButtonPressACB}>
             Login
+        </button>
+        <button class="button" style="float:right"
+        onClick={onRegisterButtonPressACB}>
+            Register
         </button>
     </div>
     );
